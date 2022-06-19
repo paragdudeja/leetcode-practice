3cac9186a3ea6e4f8d799338e98a1548562718ef
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-19 19:45:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.69%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,24 +1,6 @@
 class Solution {
-public:
-    int dp[1001];
-    bool solve(int n) {
-        if(dp[n] != -1) return dp[n];
-        if(n == 1) {
-            return dp[n] = false;
-        }
-        else {
-            for(int i = 1; i*i <= n; i++) {
-                if(n%i == 0) {
-                    if(solve(n-i) == 0) return dp[n] = true;
-                    if(i != 1 && solve(n - (n/i))==0) return dp[n] = true;
-                }
-            }
-        }
-        return dp[n] = false;
-    }
-    
+public:   
     bool divisorGame(int n) {
-        memset(dp, -1, sizeof dp);
-        return solve(n);
+        return n%2 == 0;
     }
 };
