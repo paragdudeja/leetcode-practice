6f649369c50064e7d7194336a660cd9297f6d9fd
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-20 16:31:09 +0530

    Time: 11 ms (22.31%), Space: 9.2 MB (78.93%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int i;
-        bool flag  = true;
-        for(i = 0; i < strs[0].size(); i++) {
+        for(int i = 0; i < strs[0].size(); i++) {
             for(const string &word: strs) {
-                if(word.length() < i || word[i] != strs[0][i]) {
-                    flag = false;
-                    break;
+                if(word.length() <= i || word[i] != strs[0][i]) {
+                    return strs[0].substr(0, i);
                 }
             }
-            if(!flag) break;
         }
-        return strs[0].substr(0, i);
+        return strs[0];
     }
 };
