d2027f4fb8377903db3e56d688b32d2eb12297b3
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-20 18:09:18 +0530

    Time: 6 ms (100.00%), Space: 6.8 MB (100.00%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int z=0;
+        int len=0;
         //count zero
-        for(char c:s)if(c=='0')z++;
+        for(char c:s)
+            if(c=='0')
+                len++;
 
-        int num=0, curr=1, len=0;
+        int num=0, curr=1;
 		//take as many ones from right as possible until they do not make the num>k
         for(int i=s.size()-1;i>=0;i--){    
             if(num+curr>k) break;
             if(s[i]=='1') num += curr;
-            else  z--; //already taken zeros should be redued from zeros count
+            else  len--; //already taken zeros should be redued from zeros count
             curr <<= 1;
             len++;
         }
         
-        return len+z;
+        return len;
     }
 };
