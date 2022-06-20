d0ed1210fc7e9c8b08df04ab97134ede4412f3d9
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-20 16:25:56 +0530

    Time: 33 ms (52.54%), Space: 16.7 MB (52.40%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    // Greedy
+    // TC : O(N)
+    // SC : O(1)
+    
     int jump(vector<int>& nums) {
-        int jumps = 0, currentJumpEnd = 0, farthest = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-             farthest = max(farthest, i + nums[i]);
-             if (i == currentJumpEnd) {
+        int jumps = 0, currentReach = 0, maxReach = 0;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            maxReach = max(maxReach, i + nums[i]);
+            if(i == currentReach) {
                 jumps++;
-                currentJumpEnd = farthest;
+                currentReach = maxReach;
             }
         }
         return jumps;
