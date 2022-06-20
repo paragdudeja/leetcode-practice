d5dc69407adc8d1939eb7402704a36f6ab1eb38c
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-20 11:01:44 +0530

    Time: 617 ms (5.01%), Space: 25.6 MB (6.85%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        
-        int diff = INT_MAX;
-        int ans = -1;
-        for(int first = 0; first < n; first++) {
-            int second = first + 1;
-            int third = n - 1;
-            while(second < third) {
-                int sum = nums[first] + nums[second] + nums[third];
-                if(sum == target) {
-                    diff = 0;
-                    ans = sum;
-                    while(second < third && nums[second] == nums[second+1]) second++;
-                    while(second < third && nums[third] == nums[third-1]) third--;
-                    second++;
-                    third--;
-                }
-                else if(sum < target) {
-                    if(target - sum < diff) {
-                        diff = target - sum;
-                        ans = sum;
-                    }
-                    second++;
-                }
-                else {
-                    if(sum - target < diff) {
-                        diff = sum - target;
-                        ans = sum;
-                    }
-                    third--;
-                }
+    if(nums.size() < 3) return 0;
+    int closest = nums[0]+nums[1]+nums[2];
+    sort(nums.begin(), nums.end());
+    for(int first = 0 ; first < nums.size()-2 ; ++first) {
+        if(first > 0 && nums[first] == nums[first-1]) continue;
+        int second = first+1;
+        int third = nums.size()-1;            
+        while(second < third) {
+            int curSum = nums[first]+nums[second]+nums[third];
+            if(curSum == target) return curSum;
+            if(abs(target-curSum)<abs(target-closest)) {
+                closest = curSum;
             }
-            while(first+1 < n && nums[first] == nums[first+1]) first++;
+            if(curSum > target) {
+                --third;
+            } else {
+                ++second;
+            }
         }
-        
-        return ans;
     }
+    return closest;
+}
 };
