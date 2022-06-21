5b4f2b1fc0bd077eb05b563ab98a327cc0672577
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-21 17:26:30 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (69.52%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    // TC : O(2^N)
-    // SC : O(N * 2^N)
+    // TC : O(2^N * N)
+    // SC : O(N)
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
+        vector<vector<int>> result;
         vector<int> current;
-        solve(0, nums, current, ans);
-        return ans;
+        solve(0, nums, current, result);
+        return result;
     }
     
-    void solve(int idx, vector<int> &nums, vector<int> &current, vector<vector<int>> &ans) {
-        if(idx == nums.size()) { // Base case
-            ans.push_back(current);
+    void solve(int index, vector<int> &nums, vector<int> &current, vector<vector<int>> &result) {
+        if(index == nums.size()) {
+            result.push_back(current);
             return;
         }
         
         // Pick
-        current.push_back(nums[idx]);
-        solve(idx + 1, nums, current, ans);
-        current.pop_back();
+        current.push_back(nums[index]); // Add to end the number at current index
+        solve(index + 1, nums, current, result); // Genrate combinations after picking current
+        current.pop_back(); // Remove last item after coming back
         
         // Not Pick
-        solve(idx + 1, nums, current, ans);
+        solve(index + 1, nums, current, result); // / Genrate combinations without picking current
     }
 };
