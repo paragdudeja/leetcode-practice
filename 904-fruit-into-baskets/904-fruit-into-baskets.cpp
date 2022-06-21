e43f727c94ea0c4f285bc7fc2f48aa5b8780e69d
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-21 15:08:12 +0530

    Time: 137 ms (92.71%), Space: 66.2 MB (83.73%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,51 +1,31 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int mx = 0;
-        int start = 0;
-        int n = fruits.size();
-        int type1 = -1, type2 = -1;
-        int count1 = 0, count2 = 0;
+        int lastFruit = -1;
+        int secondLastFruit = -1;
+        int lastFruitCount = 0;
+        int currMax = 0;
+        int maxFruits = 0;
         
-        for(int i = 0; i < n; i++) {
-            if(type1 == fruits[i]) {
-                count1++;
+        for (const int &fruit : fruits) {
+            if (fruit == lastFruit || fruit == secondLastFruit)
+                currMax++;
+            else
+                currMax = lastFruitCount + 1; // last fruit + new fruit
+            
+            if (fruit == lastFruit)
+                lastFruitCount++;
+            else
+                lastFruitCount = 1; 
+            
+            if (fruit != lastFruit) {
+                secondLastFruit = lastFruit;
+                lastFruit = fruit;
             }
-            else if(type2 == fruits[i]) {
-                count2++;
-            }
-            else if(type1 == -1) {
-                type1 = fruits[i];
-                count1 = 1;
-            }
-            else if(type2 == -1) {
-                type2 = fruits[i];
-                count2 = 1;
-            }
-            else {
-                while(true) {
-                    if(fruits[start++] == type1) {
-                        if(count1 == 1) {
-                            type1 = fruits[i];
-                            break;
-                        }
-                        else {
-                            count1--;
-                        }
-                    }
-                    else {
-                        if(count2 == 1) {
-                            type2 = fruits[i];
-                            break;
-                        }
-                        else {
-                            count2--;
-                        }
-                    }
-                }
-            }
-            mx = max(mx, i - start + 1);
+            
+            maxFruits = max(maxFruits, currMax);
         }
-        return mx;
+        
+        return maxFruits;
     }
 };
