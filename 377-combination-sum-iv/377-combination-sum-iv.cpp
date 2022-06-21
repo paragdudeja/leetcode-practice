37a616812647b1a186b44a688b31a4bff98996ce
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-21 19:56:57 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (32.28%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<unsigned int> dp(target+1, 0);    //Using Unsigned int because some state of dp can have very large number of ways.
-        dp[0]=1;
+        vector<unsigned int> dp(target + 1);
+        dp[0] = 1;
         
-        for(int i=1; i<=target; i++)
-        {
-            for(int num:nums)
-            {
-                if(i>=num)
-                    dp[i]+=dp[i-num];
+        for(int i = 1; i <= target; i++) {
+            for(int &num: nums) {
+                if(i >= num) {
+                    dp[i] += dp[i - num];
+                }
             }
         }
         return dp[target];
