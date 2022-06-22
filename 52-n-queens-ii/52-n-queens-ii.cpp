536eca0378f4e0769f1cb7cd53366fb326890649
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-22 12:12:16 +0530

    Time: 10 ms (39.87%), Space: 6.1 MB (77.05%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-        string row;
-        for(int i = 0; i < n; i++)
-            row.push_back('.');
-        vector<string> board;
-        for(int i = 0; i < n; i++)
-            board.push_back(row);
+        vector<vector<bool>> board(n, vector<bool>(n));
         return solve(0, n, board);
     }
     
-    int solve(int col, int n, vector<string> &board) {
+    int solve(int col, int n, vector<vector<bool>> &board) {
         if(col == n) {
             return 1;
         }
@@ -18,29 +13,29 @@ public:
         int ans = 0;
         for(int row = 0; row < n; row++) {
             if(isSafe(row, col, n, board)) {
-                board[row][col] = 'Q';
+                board[row][col] = 1;
                 ans += solve(col + 1, n, board);
-                board[row][col] = '.';
+                board[row][col] = 0;
             }
         }
         return ans;
     }
     
-    bool isSafe(int row, int col, int n, vector<string> &board) {
+    bool isSafe(int row, int col, int n, vector<vector<bool>> &board) {
         for(int j = 0; j < col; j++){
-            if(board[row][j] == 'Q') {
+            if(board[row][j]) {
                 return false;
             }
         }
         
         for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if(board[i][j] == 'Q') {
+            if(board[i][j]) {
                 return false;
             }
         }
         
         for(int i = row, j = col; i < n && j >= 0; i++, j--) {
-            if(board[i][j] == 'Q') {
+            if(board[i][j]) {
                 return false;
             }
         }
