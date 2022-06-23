161ee46ea1d1ba9a41d45e49cf46aa3d9f6ae789
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-23 16:43:32 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (97.29%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if(n == 1) return 1;
+        int oneStep = 1, twoStep = 1;
+        for(int i = 2; i <= n; i++) {
+            int currentWays = oneStep + twoStep;
+            twoStep = oneStep;
+            oneStep = currentWays;
+        }
+        return oneStep;
+    }
+    
+    /*
+    int climbStairs(int n) {
         vector<int> dp(n+1);
         dp[0] = dp[1] = 1;
         for(int i = 2; i <= n; i++)
             dp[i] = dp[i-1] + dp[i-2];
         return dp[n];
     }
+    */
 };
