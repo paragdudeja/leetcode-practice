b5e472898b7b48fd10b901e8b4aacfc3cbd80e58
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-24 01:36:28 +0530

    Time: 389 ms (42.37%), Space: 56.2 MB (58.77%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -3,24 +3,16 @@ public:
     int scheduleCourse(vector<vector<int>>& courses) {
         sort(courses.begin(), courses.end(), [](const auto &course1, const auto &course2){
             return course1[1] < course2[1];
-        });    
+        });
         
-        priority_queue<int> pq; // max heap
+        priority_queue<int> pq;
         int days = 0;
-        for(const auto &course: courses) {
-            if(days + course[0] <= course[1]) {
-                days += course[0];
-                pq.push(course[0]);
-            }
-            else if(!pq.empty() && pq.top() > course[0]) {
+        for(auto &course : courses) {
+            days += course[0];
+            pq.push(course[0]);
+            if(days > course[1]) {
                 days -= pq.top();
                 pq.pop();
-                
-                pq.push(course[0]);
-                days += course[0];
-            }
-            else {
-                // reject
             }
         }
         return pq.size();
