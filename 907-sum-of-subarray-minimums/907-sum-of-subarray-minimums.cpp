1cb613a9c06a4fb811105c3894098b8744e71e14
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-24 10:18:54 +0530

    Time: 169 ms (26.57%), Space: 43 MB (41.64%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,3 +1,4 @@
+#define MOD 1000000007
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
@@ -8,10 +9,9 @@ public:
         long long sum = 0;
         
         for(int i = 0; i < n; i++) {
-            int leftCount = i - leftBound[i];
-            int rightCount = rightBound[i] - i;
-            sum += (long long)arr[i] * leftCount * rightCount;
-            sum %= 1000000007;
+            long long prod = (leftBound[i]*rightBound[i])%MOD;
+            prod = (prod*arr[i])%MOD;
+            sum = (sum + prod)%MOD;
         }
         return sum;
     }
@@ -23,7 +23,7 @@ public:
         for(int i = 0; i < n; i++) {
             while(!st.empty() && arr[st.top()] >= arr[i])
                 st.pop();
-            prev[i] = st.empty() ? -1 : st.top();
+            prev[i] = st.empty() ? i+1 : i - st.top();
             st.push(i);
         }
         
@@ -37,7 +37,7 @@ public:
         for(int i = n-1; i >= 0; i--) {
             while(!st.empty() && arr[st.top()] > arr[i])
                 st.pop();
-            next[i] = st.empty() ? n : st.top();
+            next[i] = st.empty() ? n-i : st.top() - i;
             st.push(i);
         }
         
