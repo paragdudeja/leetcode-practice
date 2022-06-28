a553193f05f2aefb50e9bbab41564dcaab286318
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-28 17:36:24 +0530

    Time: 25 ms (46.64%), Space: 14.2 MB (85.69%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,8 +10,7 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return nullptr;
-        if(root == p || root == q) return root;
+        if(!root || root == p || root == q) return root;
         
         TreeNode *parent1 = lowestCommonAncestor(root->left, p, q);
         TreeNode *parent2 = lowestCommonAncestor(root->right, p, q);
@@ -19,4 +18,40 @@ public:
         if(parent1 && parent2) return root;
         return parent1 ? parent1 : parent2;
     }
+    
+    /*
+    // TC : O(N)
+    // SC : O(N)
+    // Stack 
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<int> path1;
+        if(!buildPath(root, p->val, path1))
+            return nullptr;
+        
+        vector<int> path2;
+        if(!buildPath(root, q->val, path2))
+            return nullptr;
+        
+        int i;
+        for(i = 0; i + 1 < path1.size() && i + 1 < path2.size(); i++) 
+            if(path1[i+1] != path2[i+1])
+                break;
+            
+        return new TreeNode(path1[i]);
+    }
+    
+    bool buildPath(TreeNode *root, int val,  vector<int> &path) {
+        if(!root) return false;
+        
+        path.push_back(root->val);
+        if(root->val == val)
+            return true;
+        
+        if(buildPath(root->left, val, path) || buildPath(root->right, val, path))
+            return true;
+        
+        path.pop_back();
+        return false;
+    }
+    */
 };
