3a3710ff43bd840396dafbc09f0969f89ca57a69
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-28 17:10:52 +0530

    Time: 292 ms (5.11%), Space: 45.1 MB (5.02%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,37 @@
+// struct Result {
+//     int max_num;
+//     int sum;
+//     Result(int max_num, int sum) : max_num(max_num), sum(sum) { }
+// };
+
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, {-1,-1}));
+        vector<int> solution = solve(0, n-1, arr, dp);
+        return solution[1];
+    }
+    
+    vector<int> solve(int left, int right, vector<int> &arr, vector<vector<vector<int>>> &dp) {
+        if(dp[left][right][0] != -1) return dp[left][right];
+        if(left > right) {
+            return {1, 0};
+        }
+        if(left == right) {
+            return {arr[left], 0};
+        }
+        vector<int> min_res{-1, -1};
+        for(int i = left; i < right; i++) {
+            vector<int> left_res = solve(left, i, arr, dp);
+            vector<int> right_res = solve(i+1, right, arr, dp);
+            vector<int> res = {max(left_res[0], right_res[0]), left_res[1] + right_res[1] 
+                + left_res[0]*right_res[0]};
+            if(min_res[0] == -1)
+                min_res = res;
+            else if(min_res[1] > res[1])
+                min_res = res;
+        }
+        return dp[left][right] = min_res;
+    }
+};
