b63b79ffa8d16c81fcf13abb442a266e2614a7a0
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-06-28 17:26:54 +0530

    Time: 81 ms (44.29%), Space: 6.7 MB (94.28%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    // TC : O(N^2)
+    // SC : O(N)
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<int> prev(n+1), curr(n+1);
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == s[n - j]) {
+                    curr[j] = 1 + prev[j - 1];
+                }
+                else {
+                    curr[j] = max(prev[j], curr[j - 1]);
+                }
+            }
+            prev = curr;
+        }
+        int lps = prev[n];
+        return n - lps;
+    }
+    
+    /*
+    // TC : O(N^2)
+    // SC : O(N^2)
     int minInsertions(string s) {
         int n = s.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1));
@@ -18,4 +42,5 @@ public:
         int res = n - lps;
         return res;
     }
+    */
 };
