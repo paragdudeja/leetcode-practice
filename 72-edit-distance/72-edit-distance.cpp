66e3eeef0c8f70dcb1738b52cb9305195803fa3c
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-01 15:50:19 +0530

    Time: 14 ms (74.34%), Space: 7.3 MB (74.39%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,10 +3,31 @@ public:
     int minDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n, m, s, t, dp);
+        
+        int dp[n+1][m+1];
+        
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(i == 0)
+                    dp[i][j] = j;
+                else if(j == 0)
+                    dp[i][j] = i;
+                else {
+                    if(s[i-1] == t[j-1])
+                        dp[i][j] = dp[i-1][j-1];
+                    else
+                        dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                }
+            }
+        }
+        
+        return dp[n][m];
+        
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return solve(n, m, s, t, dp);
     }
     
+    /*
     int solve(int n, int m, string &s, string &t, vector<vector<int>> &dp) {
         if(n == 0 || m == 0)
             return max(m, n);
@@ -22,4 +43,5 @@ public:
         
         return dp[n][m] = min({op1, op2, op3});
     }
+    */
 };
