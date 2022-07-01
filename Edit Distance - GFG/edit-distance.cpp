7d7a96238b7fae339e5e38e3bd8f918820c94522
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-01 15:43:28 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        /*
+        int dp[n+1][m+1];
+        
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                
+            }
+        }
+        */
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(n, m, s, t, dp);
+    }
+    
+    int solve(int n, int m, string &s, string &t, vector<vector<int>> &dp) {
+        if(n == 0 || m == 0)
+            return max(m, n);
+        
+        if(dp[n][m] != -1) return dp[n][m];
+        
+        if(s[n-1] == t[m-1])
+            return dp[n][m] = solve(n-1, m-1, s, t, dp);
+        
+        int op1 = 1 + solve(n-1, m, s, t, dp);
+        int op2 = 1 + solve(n, m-1, s, t, dp);
+        int op3 = 1 + solve(n-1, m-1, s, t, dp);
+        
+        return dp[n][m] = min({op1, op2, op3});
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
