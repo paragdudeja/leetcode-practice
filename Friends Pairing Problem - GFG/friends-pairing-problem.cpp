01344bd489fbbcad1254fd65bf4bbd8845550dd4
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-02 01:41:48 +0530

    Added solution - LeetHub

--- Friends Pairing Problem - GFG/friends-pairing-problem.cpp
+++ Friends Pairing Problem - GFG/friends-pairing-problem.cpp
@@ -8,14 +8,26 @@ class Solution
 public:
     int countFriendsPairings(int n) 
     { 
-        long long dp[n+1];
+        
+        long long prev1 = 1, prev2 = 1;
         const long long MOD = 1e9 + 7;
+        
+        for(int i = 2; i <= n; i++) {
+            long long curr = (prev1 + ((i-1)*prev2)%MOD)%MOD;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+        
+        /*
+        long long dp[n+1];
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2; i <= n; i++) {
             dp[i] = (dp[i-1] + ((i-1)*dp[i-2])%MOD)%MOD;
         }
         return dp[n];
+        */
         
     }
 };    
