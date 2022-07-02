4960be3caa41a927defbcaee4320016bc521aa1e
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-02 22:31:12 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -9,8 +9,9 @@ class Solution{
     public:
     long long countWays(int n, int k){
         if(n == 1) return k;
-        long long dp[n+1][2];
         const long long mod = 1e9 + 7;
+        /*
+        long long dp[n+1][2];
         dp[0][0] = dp[0][1] = 1;
         dp[1][0] = dp[1][1] = k;
         dp[2][0] = k;
@@ -20,6 +21,17 @@ class Solution{
             dp[i][1] = (((dp[i-1][0] + dp[i-1][1])%mod) * (k-1))%mod; 
         }
         return (dp[n][0] + dp[n][1])%mod;
+        */
+        long long same = k*1;
+        long long diff = (k*(k-1))%mod;
+        long long total = (same + diff)%mod;
+        
+        for(int i = 3; i <= n; i++) {
+            same = diff;
+            diff = (total * (k-1))%mod;
+            total = (same + diff)%mod;
+        }
+        return total;
     }
 };
 
