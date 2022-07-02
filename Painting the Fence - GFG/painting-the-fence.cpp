7257534fc9f760155c37ff6ff7875f4ced163e5a
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-02 22:27:12 +0530

    Added solution - LeetHub

---
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long countWays(int n, int k){
+        if(n == 1) return k;
+        long long dp[n+1][2];
+        const long long mod = 1e9 + 7;
+        dp[0][0] = dp[0][1] = 1;
+        dp[1][0] = dp[1][1] = k;
+        dp[2][0] = k;
+        dp[2][1] = k * (k-1);
+        for(int i = 3; i <= n; i++) {
+            dp[i][0] = (dp[i-1][1])%mod;
+            dp[i][1] = (((dp[i-1][0] + dp[i-1][1])%mod) * (k-1))%mod; 
+        }
+        return (dp[n][0] + dp[n][1])%mod;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		Solution ob;
+		cout<<ob.countWays(n,k)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
