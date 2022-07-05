a96cdd2a63995f217b4f2313bc9c611378715621
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-05 15:59:49 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,25 +9,22 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int arr[], int i, int j, int dp[][101]) {
-        if(i >= j)
-            return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        int ans = INT_MAX;
-        for(int k=i; k<j; k++) {
-            int temp = arr[i-1] * arr[k] * arr[j] + solve(arr, i, k, dp) + solve(arr, k+1, j, dp);
-            ans = min(ans, temp);
-        }
-        
-        return dp[i][j] = ans;
-    }
-
     int matrixMultiplication(int N, int arr[])
     {
-        int dp[101][101];
-        memset(dp, -1, sizeof dp);
+        vector<vector<int>> dp(N, vector<int>(N, -1));
         return solve(arr, 1, N-1, dp);
+    }
+    
+    int solve(int arr[], int l, int r, vector<vector<int>> &dp) {
+        if(l >= r) return 0;
+        if(dp[l][r] != -1) return dp[l][r];
+        int ans = INT_MAX;
+        
+        for(int i = l; i < r; i++) {
+            int temp = arr[l-1] * arr[i] * arr[r] + solve(arr, l, i, dp) + solve(arr, i+1, r, dp);
+            ans = min(ans, temp);
+        }
+        return dp[l][r] = ans;
     }
 };
 
