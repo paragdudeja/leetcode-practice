d042226f581264ea7403d5e4b15085cc67915873
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-05 18:39:02 +0530

    Added solution - LeetHub

--- Leaders in an array - GFG/leaders-in-an-array.cpp
+++ Leaders in an array - GFG/leaders-in-an-array.cpp
@@ -13,7 +13,7 @@ class Solution{
     public:
     vector<int> leaders(int a[], int n){
         // Code here
-        int mx = -1;
+        int mx = 0;
         vector<int> ans;
         for(int i = n-1; i >= 0; i--) {
             if(a[i] >= mx) {
