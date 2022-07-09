637e2ee997e178b24ecc094664efad744a19ba96
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-10 02:51:51 +0530

    Time: 274 ms (52.45%), Space: 96.6 MB (55.13%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
+    // TC : O(N)
+    // SC : O(1) 
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
-        int sum = 0;
         mp[0] = -1;
+        int sum = 0;
+        
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
-            int rem = sum % k;
-            if(mp.count(rem) == 0) {
-                mp[rem] = i;
+            int remainder = sum % k;
+            if(mp.find(remainder) == mp.end()) {
+                mp[remainder] = i;
             }
-            else if(i - mp[rem] > 1) {
+            else if(i - mp[remainder] > 1) {
                 return true;
             }
-            
         }
         return false;
     }
+    
+    /*
+    Brute, all subarrays
+    TC : O(N^2)
+    SC : O(1) 
+    */
 };
