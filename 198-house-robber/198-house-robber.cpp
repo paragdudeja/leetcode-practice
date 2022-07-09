2bfc4960c0db9bae19c7a3a9a8b1a1666c56f9ce
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-10 02:45:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (47.13%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     // TC: O(N)
+    // SC  O(1)
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev1 = 0, prev2 = 0, current = 0;
+        for(int i = 0; i < n; i++) {
+            current = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+    /*
+    // TC: O(N)
     // SC  O(N)
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -11,4 +24,5 @@ public:
         }
         return dp[n];
     }
+    */
 };
