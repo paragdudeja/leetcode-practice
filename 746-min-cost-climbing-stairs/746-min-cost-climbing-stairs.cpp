1bcc050a9e5e810737a1a4325fd3ab94d1ba9c3e
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-10 11:17:05 +0530

    Time: 12 ms (42.10%), Space: 13.7 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,14 +4,30 @@ public:
         cost.push_back(0);
         
         int n = cost.size();
-        vector<int> dp(n+1, INT_MAX);
+        int prev1 = cost[0], prev2 = 0, current = 0;
+        for(int i = 1; i < n; i++) {
+            current = min(prev1, prev2) + cost[i];
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+    
+    /*
+    // TC : O(N)
+    // SC : O(N)
+    int minCostClimbingStairs(vector<int>& cost) {
+        cost.push_back(0);
+        
+        int n = cost.size();
+        vector<int> dp(n+1);
         dp[0] = 0;
         dp[1] = cost[0];
-        dp[2] = cost[1];
         
-        for(int i = 2; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             dp[i+1] = min(dp[i], dp[i-1]) + cost[i];
         }
         return dp[n];
     }
+    */
 };
