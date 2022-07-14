f06d66dfe0377b48aee13b3b7c25987bd94d7770
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-15 01:08:34 +0530

    Time: 305 ms (44.20%), Space: 102.5 MB (88.28%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct NodeStatus {
+    int sum;
+    bool isBST;
+    int maxLeft;
+    int minLeft;
+};
+
+class Solution {
+public:
+    int maxSumBST(TreeNode* root) {
+        int ans = 0;
+        helper(root, ans);
+        return ans;
+    }
+    
+    NodeStatus helper(TreeNode *root, int &ans) {
+        if(!root) return {0, true, INT_MIN, INT_MAX};
+        
+        NodeStatus left = helper(root->left, ans);
+        NodeStatus right = helper(root->right, ans);
+        
+        int sum = root->val + left.sum + right.sum;
+        bool isBST = left.isBST && right.isBST && root->val > left.maxLeft  && root->val < right.minLeft;
+        if(isBST) ans = max(ans, sum);
+        int maxLeft = max(root->val, right.maxLeft);
+        int minLeft = min(root->val, left.minLeft);
+        
+        return {sum, isBST, maxLeft, minLeft};
+    }
+};
