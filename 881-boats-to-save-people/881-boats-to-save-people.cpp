3ebc174627a07d7e3b2408d5dc756a80948ca4d7
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-15 12:12:48 +0530

    Time: 483 ms (5.07%), Space: 41.9 MB (89.55%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int left = 0, right = people.size() - 1;
+        int count = 0;
+        while(left < right) {
+            cout << left << ' ' << right << endl;
+            if(people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            count++;
+        } 
+        return count + (left==right);
+    }
+};
