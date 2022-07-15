9dfbd1bc11c69cd620c8b811fad20b5898f932e0
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-15 12:12:59 +0530

    Time: 92 ms (89.24%), Space: 42 MB (53.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -5,7 +5,7 @@ public:
         int left = 0, right = people.size() - 1;
         int count = 0;
         while(left < right) {
-            cout << left << ' ' << right << endl;
+            // cout << left << ' ' << right << endl;
             if(people[left] + people[right] <= limit) {
                 left++;
             }
