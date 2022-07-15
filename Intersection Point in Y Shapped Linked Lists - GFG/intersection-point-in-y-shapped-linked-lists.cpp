faa629032ce19062700c254e67652541974e91ad
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-16 00:40:26 +0530

    Added solution - LeetHub

--- Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.cpp
+++ Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.cpp
@@ -87,8 +87,9 @@ int intersectPoint(Node* head1, Node* head2)
     Node *ptr1 = head1, *ptr2 = head2;
     while(ptr1 != ptr2) {
         ptr1 = ptr1->next;
-        if(ptr1 == NULL) ptr1 = head2;
         ptr2 = ptr2->next;
+        if(ptr1 == ptr2) break;
+        if(ptr1 == NULL) ptr1 = head2;
         if(ptr2 == NULL) ptr2 = head1;
     }
     
