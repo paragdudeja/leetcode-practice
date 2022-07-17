b2b33ae29f931ca0370d843519509ae12835d089
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-17 21:23:02 +0530

    Time: 148 ms (99.53%), Space: 75.4 MB (91.62%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     // Using monototonic stack
+    // TC : O(N) One pass
+    // SC : O(N)
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = 0;
+        stack<int> st;
+        int n = heights.size();
+        for(int i = 0; i <= n; i++) {
+            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = 0;
+                if(st.empty()) width = i;
+                else width = i - st.top() - 1;
+                maxArea = max(maxArea, height * width);                
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+    /*
+    // Using monototonic stack
     // TC : O(N)
     // SC : O(N)
     
@@ -46,4 +67,5 @@ public:
         }
         return nextSmaller;
     }
+    */
 };
