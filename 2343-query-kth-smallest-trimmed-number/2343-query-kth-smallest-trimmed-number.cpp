248ee23636be6619ddadc51394bfb051c1d206fd
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-17 14:48:37 +0530

    Time: 1980 ms (11.11%), Space: 421 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector <pair <string,int> > v[110];
-        for (int i=1; i<=nums[0].size(); i++){
-            for (int j=0; j<nums.size(); j++){
-                v[i].push_back({nums[j].substr(nums[0].size()-i,i),j});
+        vector<int> res;
+        int n = nums[0].size();
+        
+        for(const auto &q: queries) {
+            int trim = q[1];
+            int k = q[0];
+            int start = n-trim;
+            
+            vector<pair<string, int>> v;
+            int i = 0;
+            for(const auto &num: nums) {
+                string curr = num.substr(start);
+                v.push_back({curr, i++});
             }
-            sort(v[i].begin(),v[i].end());
+            sort(v.begin(), v.end(), [](const auto &v1, const auto &v2){
+                if(v1.first == v2.first) return v1.second < v2.second;
+                for(int i = 0; i < v1.first.size(); i++) {
+                    if(v1.first[i] < v2.first[i]) return true;
+                    else if(v1.first[i] > v2.first[i]) return false;
+                }
+                return v1.second < v2.second;
+            });
+            res.push_back(v[k-1].second);
         }
-        vector <int> ans;
-        for (auto i:queries){
-            ans.push_back(v[i[1]][i[0]-1].second);
-        }
-        return ans;
+        // cout << endl;
+        return res;
     }
 };
