6927a06951b4e8a79764467f10f62327f4753f57
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-18 19:04:27 +0530

    Time: 31 ms (27.78%), Space: 19.8 MB (55.93%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n-1, leftMax = 0, rightMax = 0;
+        int total = 0;
+        
+        while(left <= right) {
+            if(height[left] <= height[right]) {
+                if(height[left] >= leftMax) {
+                    leftMax = height[left];
+                }
+                else {
+                    total += leftMax - height[left];
+                }
+                left++;
+            }
+            else {
+                if(height[right] >= rightMax) {
+                    rightMax = height[right];
+                }
+                else {
+                    total += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+    }
+    /*
     // TC : O(N)
     // SC : O(N) 
     int trap(vector<int>& height) {
@@ -22,4 +50,5 @@ public:
         
         return totalWater;
     }
+    */
 };
