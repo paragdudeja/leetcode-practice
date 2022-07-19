244477dc35df69e17724085f2025a3761a981df6
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-19 17:50:02 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (42.93%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,6 +11,38 @@
  */
 class Solution {
 public:
+    
+    // Morris preorder
+    // TC : O(N)
+    // SC : O(1)
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode *curr = root;
+        while(curr) {
+            if(curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else {
+                TreeNode *prev = curr->left;
+                while(prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if(prev->right == NULL) {
+                    prev->right = curr;
+                    ans.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    /*
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(!root) return ans;
@@ -32,6 +64,8 @@ public:
         }
         return ans;
     }
+    */
+    
     
     /*
     vector<int> preorderTraversal(TreeNode* root) {
