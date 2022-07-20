2753594d04e1c2156307d4922b93df2b72b3d52d
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-20 19:23:27 +0530

    Time: 81 ms (20.57%), Space: 89.2 MB (86.23%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        if(n < m) return findMedianSortedArrays(nums2, nums1);
+        
+        int low= 0, high = m;
+        while(low <= high) {
+            int cut1 = (low + high)/2;
+            int cut2 = (m+n+1)/2 - cut1;
+            
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == m ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n ? INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1) {
+                if((m+n)&1) 
+                    return max(left1, left2);
+                else 
+                    return (max(left1, left2) + min(right1, right2))/2.0;
+            }
+            else if(left1 > right2) {
+                high = cut1-1;
+            }
+            else {
+                low = cut1+1;
+            }
+        }
+        return 0.0;
+    }
+    
+    /*
     // TC : O(M + N)
     // SC : O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -37,10 +70,11 @@ public:
             ++j;
             ++count;
         }
-    
-        
+     
         return total & 1 ? mid2 : (mid1 + mid2)/2.0;
     }
+    */
+    
     /* 
     // TC : O(M + N)
     // SC : O(M + N)
