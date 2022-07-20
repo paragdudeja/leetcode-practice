9e08c7a323b499015665c1e58e8fdf6516859c93
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-20 10:42:04 +0530

    Time: 169 ms (80.99%), Space: 89.7 MB (66.84%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
         int n = security.size();
         vector<int> prev(n), next(n);
-        prev[0] = 0;
         for(int i = 1; i < n; ++i) {
             if(security[i] > security[i-1]) {
                 prev[i] = 0;
@@ -11,14 +10,8 @@ public:
             else {
                 prev[i] = prev[i-1] + 1;
             }
-            // cout << prev[i] << ' ';
         }
-        // for(int p: prev) {
-        //     cout << p << ' ';
-        // }
-        // cout << endl;
         
-        next[n-1] = 0;
         for(int i = n-2; i >= 0; --i) {
             if(security[i] > security[i+1]) {
                 next[i] = 0;
@@ -27,9 +20,6 @@ public:
                 next[i] = next[i+1] + 1;
             }
         }
-        // for(int p: next) {
-        //     cout << p << ' ';
-        // }
         
         vector<int> ans;
         for(int i = time; i < n - time; ++i) {
