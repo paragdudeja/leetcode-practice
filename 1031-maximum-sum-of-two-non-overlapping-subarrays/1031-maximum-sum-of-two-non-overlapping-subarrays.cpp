c94c93b61311221097c63d0319a7624d4d129553
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-21 10:23:17 +0530

    Time: 2 ms (95.10%), Space: 8.2 MB (99.51%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
+    // TC : O(N)
+    // SC : O(1)
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        return max(solve(nums, firstLen, secondLen), solve(nums, secondLen, firstLen));
+    }
+    
+    int solve(vector<int>& A, int L, int M) {
+        int n = A.size();
+        int sMMax = 0;
+        int sL = 0, sM = 0;
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            sL += A[i];
+            if(i >= L) {
+                sL -= A[i-L];
+                sM += A[i-L];
+            }
+            if(i-L >= M) {
+                sM -= A[i-L-M];
+            }
+            sMMax = max(sMMax, sM);
+            res = max(res, sL + sMMax);      
+        }
+        
+        return res;
+    }
+    
+    /*
+    // TC : O(N^2)
+    // SC : O(N)
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
         int n = nums.size();
-        
         int prefix[n+1];
         prefix[0] = 0;
         for(int i = 0; i < n; ++i) {
             prefix[i+1] = prefix[i] + nums[i];
-            // cout << prefix[i+1] << ' ';
         }
         
         int maxSum = 0;
         for(int i = firstLen; i <= n; ++i) {
-            // cout << firstLen << ' ' << (firstLen-i) << endl;
             int firstSum = prefix[i] - prefix[i - firstLen];
-            // cout << firstSum << endl;
-
             for(int j = i - firstLen; j - secondLen >= 0; --j) {
                 int secondSum = prefix[j] - prefix[j - secondLen];
                 maxSum = max(maxSum, firstSum + secondSum);
-                // cout << secondSum << ' ';
             }
-            // cout << endl;
             for(int j = n; j - secondLen >= i; j--) {
                 int secondSum = prefix[j] - prefix[j - secondLen];
                 maxSum = max(maxSum, firstSum + secondSum);
@@ -29,4 +52,5 @@ public:
         }
         return maxSum;
     }
+    */
 };
