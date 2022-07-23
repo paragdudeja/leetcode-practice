e5cbc55f432b28602d43ac31ef6bc6fd82a973fa
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-23 09:24:35 +0530

    Time: 3 ms (91.63%), Space: 6.3 MB (83.72%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
+    int dp[21][31];
     bool solve(string &s, string &p, int i, int j) {
-        cout << i << ' ' << j << endl;
+        // cout << i << ' ' << j << endl;
         if(i == -1 && j == -1) return true;
         if(i == -1) {
             for(int k = j; k >= 0; k -= 2) {
                 if(p[k] != '*') return 0;
             }
-            // cout << "Yes\n";
             return 1;
         }
         if(j == -1) return 0;
         
+        if(dp[i][j] != -1) return dp[i][j];
+        
         if(p[j] == '*') {
             if(p[j-1] == '.' ||  s[i] == p[j-1]) {
-                return solve(s, p, i-1, j) or solve(s, p, i, j-2);
+                return dp[i][j] = solve(s, p, i-1, j) or solve(s, p, i, j-2);
             }
             else {
-                return solve(s, p, i, j-2);
+                return dp[i][j] = solve(s, p, i, j-2);
             }
         }
         if(p[j] == '.' || s[i] == p[j]) 
-            return solve(s, p, i-1, j-1);
-        return 0;
+            return dp[i][j] = solve(s, p, i-1, j-1);
+        return dp[i][j] = 0;
     }
     
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof dp);
         return solve(s, p, s.size()-1, p.size()-1);
     }
 };
