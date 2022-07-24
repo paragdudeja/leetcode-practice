83e217bb648d43e890404d492c5ce274b24674a2
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-24 14:55:26 +0530

    Time: 1167 ms (18.01%), Space: 207.1 MB (30.04%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3)));
+        
+        for(int i = n-1; i >= 0; --i) {
+            for(int buy = 0; buy <= 1; ++buy) {
+                for(int cap = 1; cap <= 2; ++cap) {
+                    if(buy == 1) {
+                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+                    }
+                    else {
+                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+    
+    /*
     int dp[100001][3][2];
     int solve(int i, bool is_holding, vector<int> &prices, int k) {
         if(i == prices.size() || k == 0) {
@@ -22,4 +42,5 @@ public:
         memset(dp, -1, sizeof dp);
         return solve(0, false, prices, 2);
     }
+    */
 };
