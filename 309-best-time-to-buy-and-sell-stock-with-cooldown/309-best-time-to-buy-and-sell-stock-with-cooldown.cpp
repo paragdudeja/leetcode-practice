cd393e086352bb6be008ff71710ade821d3f1862
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-24 15:36:38 +0530

    Time: 7 ms (61.57%), Space: 11.9 MB (45.03%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,10 +5,8 @@ public:
         vector<vector<int>> dp(n+2, vector<int>(2));
         
         for(int i = n-1; i >= 0; --i) {
-            for(int buy = 0; buy <= 1; ++buy) {
-                dp[i][1] = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-                dp[i][0] = max(prices[i] + dp[i+2][1], dp[i+1][0]);
-            }
+            dp[i][1] = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+            dp[i][0] = max(prices[i] + dp[i+2][1], dp[i+1][0]);
         }
         
         return dp[0][1];
