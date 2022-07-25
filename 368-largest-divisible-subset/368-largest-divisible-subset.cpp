a956de880b3e577bee35db5153774224f37ec260
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-25 06:20:15 +0530

    Time: 164 ms (5.03%), Space: 8.7 MB (92.24%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -6,33 +6,31 @@ public:
         
         sort(nums.begin(), nums.end());
         int longest = 0;
+        int lastIndex = 0;
         int dp[n];
         for(int i = 0; i < n; ++i) {
             hash[i] = i;
             dp[i] = 1;
   
             for(int j = 0; j < i; ++j) {
-                if(nums[i] % nums[j] == 0 && dp[j]+1 > dp[i]) {
+                if((nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0) && dp[j]+1 > dp[i]) {
                     dp[i] = dp[j] + 1;
                     hash[i] = j;
                 }
             }
             
-            longest = max(longest, dp[i]);
+            if(dp[i] > longest) {
+                longest = dp[i];
+                lastIndex = i;
+            }
         }
         
         vector<int> ans;
-        
-        for(int i = 0; i < n; ++i) {
-            if(dp[i] == longest) {
-                while(ans.size() < longest) {
-                    ans.push_back(nums[i]);
-                    i = hash[i];
-                }
-            }
-            if(!ans.empty()) break;
+        while(ans.size() < longest) {
+            ans.push_back(nums[lastIndex]);
+            lastIndex = hash[lastIndex];
         }
-        
+
         reverse(ans.begin(), ans.end());
         return ans;
     }
