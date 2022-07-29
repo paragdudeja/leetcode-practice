3b4dfc6cff8ef7eed8dbffd6a4f34d2f6e654be8
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-07-29 06:28:56 +0530

    Time: 7 ms (78.35%), Space: 7.1 MB (19.33%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -5,9 +5,9 @@ public:
         vector<int> c(256, -100), d(256, -100);
         int n= s.size();
         for(int i=0; i<n; i++){
-            int diff = s[i]-t[i];
             int index1 = s[i];
             int index2 = t[i];
+            int diff = index1-index2;
 
             if(c[index1] == -100) c[index1] = diff;
             else if(c[index1]!=diff) return false;
