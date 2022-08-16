e9b71e1b76f1cfdb3683222348340f543b55989d
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-08-16 08:49:45 +0530

    Time: 31 ms (83.25%), Space: 10.6 MB (71.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int state[26];
-        int res = INT32_MAX;
-        memset(state, -1, sizeof(int)*26);
-        for(int i=0; i<s.length(); i++){
-            if(state[s[i]-'a']==-1)
-                state[s[i]-'a'] = i;
-            else
-                state[s[i]-'a'] = -2;
-        }
-        for(int i=0; i<26; i++)
-            if(state[i]>=0)
-                res = min(res, state[i]);
-        return res==INT32_MAX? -1: res;
+        int count[26] = {0};
+        for(int i=0; i<s.length(); i++)
+            count[s[i]-'a']++;
+        for(int i=0; i<s.length(); i++)
+            if(count[s[i]-'a']==1)
+                return i;
+
+        return -1;
     }
 };
