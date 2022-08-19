a2748ced643743eb27060e84835858bb8e336d46
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-08-19 21:33:03 +0530

    Time: 240 ms (17.83%), Space: 72.5 MB (10.03%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     bool isPossible(vector<int>& nums) {
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> s;
+        for(int x: nums) {
+            if(s[x-1].empty()) {
+                s[x].push(1);
+            }
+            else {
+                int len = s[x-1].top();
+                s[x-1].pop();
+                s[x].push(len+1);
+            }
+        }
+        
+        for(auto it: s) {
+            while(!it.second.empty()) {
+                if(it.second.top() < 3) return false;
+                it.second.pop();
+            }
+        }
+        return true;
+    }
+    /*
+    bool isPossible(vector<int>& nums) {
         multiset<pair<int, int>> s;
         for(int x: nums) {
             auto it = s.lower_bound({x-1, 0});
@@ -18,4 +40,5 @@ public:
         }
         return true;
     }
+    */
 };
