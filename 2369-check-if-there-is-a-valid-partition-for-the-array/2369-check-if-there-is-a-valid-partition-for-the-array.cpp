58ca27cd3327fb62d5a9e55ad6468afb1150643a
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-08-20 16:19:57 +0530

    Time: 327 ms (18.91%), Space: 84.1 MB (79.60%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
+        
         vector<bool> dp(n+1);
         dp[n] = true;
-        
-        for(int index = n-2; index >= 0; index--) {
+        dp[n-2] = nums[n-2] == nums[n-1];
+        for(int index = n-3; index >= 0; index--) {
             if(nums[index] == nums[index+1]) {
                 dp[index] = dp[index] or dp[index+2];
             }
-            if(index+2 < n) {
-                if(nums[index]==nums[index+1] && nums[index]==nums[index+2]) {
-                    dp[index] = dp[index] or dp[index+3];
-                }
-                if(nums[index]+1==nums[index+1] && nums[index]+2==nums[index+2]) {
-                    dp[index] = dp[index] or dp[index+3];
-                }
+            if(nums[index]==nums[index+1] && nums[index]==nums[index+2]) {
+                dp[index] = dp[index] or dp[index+3];
+            }
+            else if(nums[index]+1==nums[index+1] && nums[index]+2==nums[index+2]) {
+                dp[index] = dp[index] or dp[index+3];
             }
         }
         return dp[0];
