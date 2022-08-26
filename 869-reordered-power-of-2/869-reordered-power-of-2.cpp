d3927e8bc1973444b35dcdc46ee1e08f4240f854
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-08-26 08:22:19 +0530

    Time: 6 ms (50.57%), Space: 6.2 MB (45.48%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
+        vector<int> num_hash = get_hash(n);
+        for(int i = 0; i < 31; ++i) {
+            if(num_hash == get_hash(1 << i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    vector<int>  get_hash(int num) {
+        vector<int> hash(10);
+        while(num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            hash[digit]++;
+        }
+        
+        return hash;
+    }
+    
+    /*
+    bool reorderedPowerOf2(int n) {
         int t = 1;
         string s = to_string(n);
         sort(s.begin(), s.end());
@@ -16,7 +38,7 @@ public:
                 t <<= 1;
             }
         }
-        return flag;
-        
+        return flag; 
     }
+    */
 };
