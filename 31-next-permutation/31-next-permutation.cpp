a8a05197d78623ee813276375b0ca82327d63645
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-01 01:14:53 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (36.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx1 = nums.size()-2, idx2 = -1;
+        int idx1 = nums.size()-2, idx2 = nums.size()-1;
         while(idx1 >= 0 && nums[idx1] >= nums[idx1 + 1]){
             idx1--;
         }
         if(idx1 != -1) {
-            idx2 = nums.size()-1;
             while(idx2 > idx1 && nums[idx2] <= nums[idx1]) {
                 idx2--;
             }
