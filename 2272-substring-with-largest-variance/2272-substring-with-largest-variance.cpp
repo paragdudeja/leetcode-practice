948f1fe8740f45b15f123e194b5e3da4ea960ffb
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-05 11:43:25 +0530

    Time: 342 ms (23.83%), Space: 7.2 MB (39.69%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -3,16 +3,20 @@ public:
     int largestVariance(string s) {
         string r = s;
         reverse(s.begin(), s.end());
-        return max(get_max_var(s), get_max_var(r));
+        
+        vector<int> freq(256);
+        for(char &ch: s) freq[ch]++;
+        
+        return max(get_max_var(s, freq), get_max_var(r, freq));
     }
     
-    int get_max_var(string &s) {
+    int get_max_var(string &s, vector<int> &freq) {
         int max_variance = 0;
         for(char ch1 = 'a'; ch1 <= 'z'; ch1++) {
             for(char ch2 = 'a'; ch2 <= 'z'; ch2++) {
-                if(ch1 == ch2) continue;
+                if(ch1 == ch2 || freq[ch1] == 0 || freq[ch2] == 0) continue;
                 int f1 = 0, f2 = 0;
-                for(char ch: s) {
+                for(char &ch: s) {
                     if(ch == ch1) f1++;
                     if(ch == ch2) f2++; 
                     if(f1 < f2) {
