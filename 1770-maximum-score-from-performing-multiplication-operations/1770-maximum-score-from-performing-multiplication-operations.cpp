c19f434439a3e6a34da1f4250db0fa4c95148b31
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-17 00:28:16 +0530

    Time: 882 ms (27.11%), Space: 120 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
-        if (j == M.size()) return 0;
-        if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
-        return dp[i][j] = max(left, right);
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size();
+        int m = multipliers.size();
+        vector<vector<int>> dp(m+1, vector<int>(m+1, INT_MIN));
+        return solve(0, 0, nums, multipliers, dp, m, n);
     }
     
-    int maximumScore(vector<int>& nums, vector<int>& M) {   
-        int n = nums.size(), m = M.size();
-        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, M);
+    int solve(int idx, int start, vector<int> &nums, vector<int> &multipliers, vector<vector<int>> &dp, int m, int n) {
+        if(idx == m) return 0;
+        
+        if(dp[start][idx] != INT_MIN) return dp[start][idx];
+        
+        int startPick = multipliers[idx]*nums[start] + solve(idx+1, start+1, nums, multipliers, dp, m, n);
+        int endPick = multipliers[idx]*nums[n - (idx - start) - 1] + solve(idx+1, start, nums, multipliers, dp, m, n);
+        
+        return dp[start][idx] =  max(startPick, endPick);
     }
 };
