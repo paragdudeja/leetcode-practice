41f940decb28f6a0b998d92633ad1b593c39c5e9
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-17 00:30:52 +0530

    Time: 916 ms (23.90%), Space: 119.9 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        int n = nums.size();
         int m = multipliers.size();
         vector<vector<int>> dp(m+1, vector<int>(m+1, INT_MIN));
-        return solve(0, 0, n-1, nums, multipliers, dp, m, n);
+        return solve(0, 0, nums.size()-1, nums, multipliers, dp);
     }
     
-    int solve(int idx, int start, int end, vector<int> &nums, vector<int> &multipliers, vector<vector<int>> &dp, int m, int n) {
-        if(idx == m) return 0;
+    int solve(int idx, int start, int end, vector<int> &nums, vector<int> &multipliers, vector<vector<int>> &dp) {
+        if(idx == multipliers.size()) return 0;
         
         if(dp[start][idx] != INT_MIN) return dp[start][idx];
         
-        int startPick = multipliers[idx]*nums[start] + solve(idx+1, start+1, end, nums, multipliers, dp, m, n);
-        int endPick = multipliers[idx]*nums[end] + solve(idx+1, start, end-1, nums, multipliers, dp, m, n);
+        int startPick = multipliers[idx]*nums[start] + solve(idx+1, start+1, end, nums, multipliers, dp);
+        int endPick = multipliers[idx]*nums[end] + solve(idx+1, start, end-1, nums, multipliers, dp);
         
         return dp[start][idx] =  max(startPick, endPick);
     }
