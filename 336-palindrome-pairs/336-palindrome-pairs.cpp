46df0c894fd4e388fb6ecb77f68259b959bcc609
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-17 23:34:05 +0530

    Time: 1901 ms (42.83%), Space: 599.1 MB (16.33%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -0,0 +1,75 @@
+struct TrieNode {
+    TrieNode *next[26] = {};
+    int index = -1;
+    vector<int> palindromeIndexes;
+};
+
+class Solution {
+    TrieNode root; // Suffix trie
+    void add(string &s, int i) {
+        auto node = &root;
+        for (int j = s.size() - 1; j >= 0; --j) {
+            if (isPalindrome(s, 0, j)) node->palindromeIndexes.push_back(i); // A[i]'s prefix forms a palindrome
+            int c = s[j] - 'a';
+            if (!node->next[c]) node->next[c] = new TrieNode();
+            node = node->next[c];
+        }
+        node->index = i;
+        node->palindromeIndexes.push_back(i); // A[i]'s prefix is empty string here, which is a palindrome.
+    }
+    
+    bool isPalindrome(string &s, int i, int j) {
+        while (i < j && s[i] == s[j]) ++i, --j;
+        return i >= j;
+    }
+    
+public:
+    vector<vector<int>> palindromePairs(vector<string>& A) {
+        int N = A.size();
+        for (int i = 0; i < N; ++i) add(A[i], i);
+        vector<vector<int>> ans;
+        for (int i = 0; i < N; ++i) {
+            auto s = A[i];
+            auto node = &root;
+            for (int j = 0; j < s.size() && node; ++j) {
+                if (node->index != -1 && node->index != i && isPalindrome(s, j, s.size() - 1)) ans.push_back({ i, node->index }); 
+                // A[i]'s prefix matches this word and A[i]'s suffix forms a palindrome
+                node = node->next[s[j] - 'a'];
+            }
+            if (!node) continue;
+            for (int j : node->palindromeIndexes) { 
+                // A[i] is exhausted in the matching above. 
+                // If a word whose prefix is palindrome after matching its suffix with A[i], 
+                // then this is also a valid pair
+                if (i != j) ans.push_back({ i, j });
+            }
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    bool isPalindrome(string &s) {
+        int i = 0, j = s.size()-1;
+        while(i < j) {
+            if(s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+    
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        vector<vector<int>> result;
+        int n = words.size();
+        for(int i = 0; i < n; ++i) {
+            for(int j = i + 1; j < n; ++j) {
+                string s1 = words[i] + words[j];
+                if(isPalindrome(s1)) result.push_back({i, j});
+                s1 = words[j] + words[i];
+                if(isPalindrome(s1)) result.push_back({j, i});
+            }
+        }
+        return result;
+    }    
+};
+*/
