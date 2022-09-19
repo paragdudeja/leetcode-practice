89696d368603b6ae3d817e5eba3f556108111973
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-19 14:30:42 +0530

    Time: 68 ms (15.04%), Space: 19.1 MB (98.01%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+    /*
     void moveZeroes(vector<int>& nums) {
         for(int i = 0, countNonZero = 0; i < nums.size(); ++i) {
             if(nums[i] != 0) {
                 swap(nums[i], nums[countNonZero++]);
             }
         }
+    }
+    */
+    
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;  // Count of non-zero elements
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+            if (nums[i] != 0)
+                nums[count++] = nums[i]; // here count is
+                                       // incremented
+        while (count < n)
+            nums[count++] = 0;
     }
 };
