80336a6c8142264fffb6e139ea472de3fddf6651
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-21 12:39:20 +0530

    Time: 100 ms (11.92%), Space: 28.2 MB (53.75%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -3,13 +3,19 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int start = 0, sum = 0;
         int min_len = 1e6;
+        bool flag = false;
         
         for(int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
             while(sum >= target) {
-                if(min_len > i - start + 1) min_len = i - start + 1;
+                flag = true;
+                // if(min_len > i - start + 1) min_len = i - start + 1;
                 sum -= nums[start];
                 start++;
+            }
+            if(flag) {
+                flag = false;
+                if(min_len > i - start +  + 2) min_len = i - start + 2;
             }
         }
         
