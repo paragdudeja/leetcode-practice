f89e39532a26424775b46db90ac49feb2971d3c9
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-09-21 11:19:43 +0530

    Time: 5 ms (43.49%), Space: 7.6 MB (14.86%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -3,19 +3,19 @@ public:
     int splitArray(vector<int>& nums, int m) {
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
-        int ans = high;
+        // int ans = high;
         
-        while(low <= high) {
+        while(low < high) {
             int mid = low + (high - low)/2;
             if(isPossible(nums, m, mid)) {
-                ans = mid;
-                high = mid - 1;
+                // ans = mid;
+                high = mid;
             }
             else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
     
     bool isPossible(vector<int> nums, int k, int limit) {
