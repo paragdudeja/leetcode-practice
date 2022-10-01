8f337dce773515567e1b822f6d08f7e54929906f
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-10-01 21:43:48 +0530

    Time: 72 ms (9.11%), Space: 13.9 MB (39.73%) - LeetHub

---
+++ 564-find-the-closest-palindrome/564-find-the-closest-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nearestPalindromic(self, n: str) -> str:
+        l = len(n)
+        # with different digits width, it must be either 10...01 or 9...9
+        candidates = set((str(10 ** l + 1), str(10 ** (l - 1) - 1)))
+        # the closest must be in middle digit +1, 0, -1, then flip left to right
+        prefix = int(n[:(l + 1)//2])
+        for i in map(str, (prefix - 1, prefix, prefix + 1)):
+            candidates.add(i + [i, i[:-1]][l & 1][::-1])
+        candidates.discard(n)
+        return min(candidates, key=lambda x: (abs(int(x) - int(n)), int(x)))
