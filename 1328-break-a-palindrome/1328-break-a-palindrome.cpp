6a89a659980657148989ce7dbab04fd65758e168
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-10-11 00:35:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,17 +3,13 @@ public:
     string breakPalindrome(string palindrome) {
         int n = palindrome.size(); 
         if(n == 1) return ""; 
-        bool flag = false;
         for(int i = 0; i < n/2; ++i) {
             if(palindrome[i] != 'a') {
                 palindrome[i] = 'a';
-                flag = true;
-                break;
+                return palindrome;
             }
         }
-        if(!flag) {
-            palindrome[n-1] = 'b';
-        }
+        palindrome[n-1] = 'b';
         return palindrome;
     }
 };
