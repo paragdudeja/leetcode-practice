87dd85c6913c9581a83808f56fe107c75e7fe4c4
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-10-18 12:33:48 +0530

    Time: 19 ms (43.50%), Space: 6.5 MB (77.40%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        string ans = "1";
+        for(int i = 2; i <= n; ++i) {
+            string curr;
+
+            int cnt = 1;
+            for(int j = 1; j < ans.size(); ++j) {
+                if(ans[j] == ans[j-1]) {
+                    cnt++;
+                }
+                else {
+                    curr += to_string(cnt);
+                    curr.push_back(ans[j-1]);
+                    cnt = 1;
+                }
+            }
+            curr += to_string(cnt);
+            curr.push_back(ans.back());
+
+            // cout << curr << endl;
+
+            ans = curr;
+        }
+        return ans;
+    }    
+    
+    /*
+    string countAndSay(int n) {
         string ans = solve(n);
         ans.pop_back();
         return ans;
@@ -23,4 +50,5 @@ public:
         tmp.push_back('#');
         return tmp;
     }
+    */
 };
