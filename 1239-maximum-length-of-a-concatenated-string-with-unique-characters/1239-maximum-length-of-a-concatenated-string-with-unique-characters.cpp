050775951157ded3286cc3931a13311d1604646e
paragdudeja <32008907+paragdudeja@users.noreply.github.com>
2022-10-24 23:44:51 +0530

    Time: 268 ms (41.29%), Space: 12.1 MB (70.20%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        int tablesize = (1<<n);
+        vector<int> dp(tablesize);
+        
+        int ans = 0;
+        for(int i = 1; i < tablesize; ++i) {
+            for(int j = 0; j < n; ++j) {
+                bool flag = false;
+                if(i & (1<<j)) {
+                    for(char ch: arr[j]) {
+                        if(dp[i] & (1<<(ch - 'a'))) {
+                            dp[i] = 0;
+                            flag = true;
+                            break;
+                        }
+                        dp[i] = dp[i] | (1<<(ch - 'a'));
+                        
+                    }
+                    if(flag) break;
+                }
+            }
+            ans = max(ans, __builtin_popcount(dp[i]));
+        }
+        
+        
+        return ans;
+    }
+};
